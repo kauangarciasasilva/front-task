07354ad8668f4438e059c116ae3b2cd794a8a638
Kauan Garcia Dutra Da Silva <TRI0106034@aluno.firjansenaisesi.com.br>
2023-03-20 21:34:19 -0300

    aula

--- src/home/views/components/task-tab-group.tsx
+++ src/home/views/components/task-tab-group.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { ListGroup, Row, Col, FormCheck, Tab, Tabs, Badge } from "react-bootstrap";
+import { ListGroup, Row, Col, FormCheck, Tab, Tabs, Badge, Button } from "react-bootstrap";
 import { ArrowRight, Trash, Pencil } from 'react-bootstrap-icons';
+import { MDBBtn } from "mdb-react-ui-kit";
 interface Props{
     firstTabDescription: string;
     secondTabDescription: string;
@@ -18,59 +19,79 @@ export default class TaskTabGroup extends React.Component<Props>{
                                                         <Col sm='1'>
                                                             <FormCheck></FormCheck>                                        
                                                         </Col>                     
-                                                        <Col>
+                                                        <Col className="py-3 px-3" >
                                                             <span>No style</span>
                                                         </Col>
                                                         <Col sm='1'>
-                                                         <Trash/>
-                                                         <Pencil/>
-                                                        </Col>
-                                                       
+                                                        <Button variant="primary"className="py-0 px-0 mx-4">Editar</Button>
                                                         
-                                                       
-                                                    </Row>
-                                                </ListGroup.Item>                                    
-                                            </ListGroup>
-                                            <ListGroup>
-                                                <ListGroup.Item action variant='secondary' className="mb-2">
-                                                    <Row>
-                                                        <Col sm='1'>
-                                                            <FormCheck></FormCheck>                                        
-                                                        </Col>                     
-                                                        <Col>
-                                                            <span>No style</span>
-                                                            
-                                                        </Col>
-                                                        <Col sm='1'>
-                                                         <Trash/>
-                                                         <Pencil/>
-                                                        </Col>
-                                                        
-                                                    </Row>
-                                                </ListGroup.Item>                                    
-                                            </ListGroup>
-                                            <ListGroup>
-                                                <ListGroup.Item action variant='secondary' className="mb-2">
-                                                    <Row>
-                                                        <Col sm='1'>
-                                                            <FormCheck></FormCheck>                                        
-                                                        </Col>                     
-                                                        <Col>
-                                                            <span>No style</span>
-                                                           
-                                                           
-                                                           
-                                                          
-                                                          
-                                                        </Col>
-                                                        <Col sm='1'className="mx-5">
-                                                         <Trash/>
+                                                         
                                                          <Col>
-                                                         <Pencil/>
+                                                         
                                                          </Col>
-                                                         
-                                                         
+                                                         <Button variant="danger"className="py-0 px-0 mx-4">Delete</Button>
+                                                        
                                                         </Col>
+                                                       
+                                                        
+                                                       
+                                                    </Row>
+                                                </ListGroup.Item>                                    
+                                            </ListGroup>
+                                            <ListGroup>
+                                                <ListGroup.Item action variant='secondary' className="mb-2">
+                                                    <Row>
+                                                        <Col sm='1'>
+                                                            <FormCheck></FormCheck>                                        
+                                                        </Col>                     
+                                                        <Col className="py-3 px-3">
+                                                            <span>No style</span>
+                                                           
+                                                        </Col >
+                                                        <Col sm='1'>
+                                                        <Button variant="primary"className="py-0 px-0 mx-4">Editar</Button>
+                                                        
+                                                         
+                                                         <Col>
+                                                         
+                                                         </Col>
+                                                         <Button variant="danger"className="py-0 px-0 mx-4">Delete</Button>
+                                                        
+                                                        </Col>
+                                                        
+                                                        
+                                                    </Row>
+                                                </ListGroup.Item>                                    
+                                            </ListGroup>
+                                            <ListGroup>
+                                                <ListGroup.Item action variant='secondary' className="mb-2">
+                                                    <Row>
+                                                        <Col sm='1'>
+                                                            <FormCheck></FormCheck>                                        
+                                                        </Col>                     
+                                                        <Col className="py-3 px-3">
+                                                            <span>No style</span>
+                                                           
+                                                           
+                                                           
+                                                          
+                                                          
+                                                        </Col>
+                                                        <Col sm='1'>
+                                                        <Button variant="primary"className="py-0 px-0 mx-4">Editar</Button>
+                                                        
+                                                         
+                                                         <Col>
+                                                         
+                                                         </Col>
+                                                         <Button variant="danger"className="py-0 px-0 mx-4">Delete</Button>
+                                                        
+                                                        </Col>
+                                                        
+                                                        
+                                                        
+                                                         
+                                                      
                                                         
                                                     </Row>
                                                 </ListGroup.Item>                                    
